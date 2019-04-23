7e7528ed3a583454723fbcb283498a03ae0e569f
Jeetendra Achtani <wide.angle456@gmail.com>
2019-04-23 14:44:44 +0530

    Added notifications

--- app/src/main/java/com/app/androidkt/geofencing/GeofenceRegistrationService.java
+++ app/src/main/java/com/app/androidkt/geofencing/GeofenceRegistrationService.java
@@ -1,8 +1,12 @@
 package com.app.androidkt.geofencing;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -31,6 +35,22 @@ public class GeofenceRegistrationService extends IntentService {
             if (transaction == Geofence.GEOFENCE_TRANSITION_ENTER && geofence.getRequestId().equals(Constants.GEOFENCE_ID_Titanium)) {
                 Toast.makeText(this, "You are inside Monitored Area ", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "You are inside Monitored Area ");
+
+                String CHANNEL_ID = Constants.GEOFENCE_ID_Titanium;
+                NotificationCompat.Builder builder =
+                        new NotificationCompat.Builder(this)
+                                .setSmallIcon(R.drawable.titanium)
+                                .setContentTitle("You entered Titanium City Center")
+                                .setContentText("Please connect to the office. ");
+
+                builder.setAutoCancel(true);
+
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                mNotificationManager.notify(0, builder.build());
+
+
             } else {
                 Toast.makeText(this, "You Have gone outside Monitored Area", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "You are outside outside Monitored Area");
